50bc7c88330c24b1bab7485861af5d64c6322d20
Hardik Bhanot <hardik_bhanot@Hardiks-MacBook-Air.local>
2025-09-16 23:27:49 +0530

    Making app production ready

--- frontend/portfolio-frontend/src/api/axiosConfig.jsx
+++ frontend/portfolio-frontend/src/api/axiosConfig.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Create a new Axios instance with a base URL
 const apiClient = axios.create({
-  baseURL: 'http://localhost:8080', // Your backend's base URL
+  baseURL: process.env.REACT_APP_API_URL, // Your backend's base URL
 });
 
 /**
